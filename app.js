cade0ac1c1881efb325d1992e589e813d9fce6fe
Ward <wardv@tirf.ca>
2022-01-17 15:49:21 -0500

    revised prompt function with questions for README

---
+++ app.js
@@ -0,0 +1,104 @@
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'confirmTableOfC',
+      message: 'Would you like to enter a table of contents?',
+      default: true
+    },
+    {
+      type: 'checkbox',
+      name: 'tableOfC',
+      message: 'What sections are in your README? (Check all that apply)',
+      choices: ['Installation', 'Usage', 'Credits', 'Badges', 'Licence'],
+      when: ({confirmTableOfC}) => confirmTableOfC
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How to install? (Required)',
+      validate: installationInput => {
+        if (installationInput) {
+          return true;
+        } else {
+          console.log('Please describe installation process!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How to use? (Required)',
+      validate: usageInput => {
+        if (usageInput) {
+          return true;
+        } else {
+          console.log('Please describe how to use it!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Who contributed? (Required)',
+      validate: creditsInput => {
+        if (creditsInput) {
+        return true;
+        } else {
+        console.log('Please describe who contributed!');
+        return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmBadges',
+      message: 'Would you like to enter badges?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'Badges',
+      message: 'Please list your badges? (Required)',
+      when: ({confirmBadges}) => confirmBadges
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'List license? (Required)',
+      validate: licenseInput => {
+        if (licenseInput) {
+        return true;
+        } else {
+        console.log('Please describe the license!');
+        return false;
+        }
+      }
+    },
+  ]);
+};
+
+promptUser()
+  .then(readmeData => {
+    return generatePage(readmeData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,58 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
-module.exports = generateMarkdown;
+// module.exports = generateMarkdown;
+
+const fs = require('fs');
+
+// writing files
+const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
+  });
+};
+
+// copying file
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'Stylesheet created!'
+      });
+    });
+  });
+};
+
+module.exports = { writeFile, copyFile };
